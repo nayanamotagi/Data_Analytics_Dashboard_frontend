b748564f860320e3d20ef7946c90d42bbf803283
nayanamotagi <nayanamotagi24@gmail.com>
2026-05-12 11:15:22 +0530

    modified  package file

---
+++ src/config/apiConfig.js
@@ -0,0 +1,11 @@
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
+export const getApiBaseUrl = () => {
+    return API_BASE_URL;
+};
+
+const apiConfig = {
+    API_BASE_URL
+};
+
+export default apiConfig;
